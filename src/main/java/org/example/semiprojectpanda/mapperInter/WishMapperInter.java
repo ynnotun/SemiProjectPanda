9817d221e509b1fd4e1ef02f4fca1bf4bd90ea9d
taehyoung809 <soona809@naver.com>
2024-06-14 17:23:22 +0900

    6/14 usagi_jjang

--- src/main/java/org/example/semiprojectpanda/mapperInter/WishMapperInter.java
+++ src/main/java/org/example/semiprojectpanda/mapperInter/WishMapperInter.java
@@ -1,7 +1,14 @@
 package org.example.semiprojectpanda.mapperInter;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.example.semiprojectpanda.dto.WishDto;
+
+import java.util.List;
 
 @Mapper
 public interface WishMapperInter {
+    //찜목록
+    @Select("select * from wish where usernum=#{usernum}") //이 사람의 찜목록을 불러와야 하니까!
+    public List<WishDto> getWishList(int usernum);
 }
