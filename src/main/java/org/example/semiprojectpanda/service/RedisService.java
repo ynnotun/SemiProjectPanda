316877fc2cc134a82be0e67659ec566b317d24d8
LBang98 <dlqudgus98@gmail.com>
2024-06-14 17:23:37 +0900

    test

---
+++ src/main/java/org/example/semiprojectpanda/service/RedisService.java
@@ -0,0 +1,25 @@
+package org.example.semiprojectpanda.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public void save(String key, String value) {
+        redisTemplate.opsForValue().set(key, value, 3, TimeUnit.MINUTES); // 3분동안 저장
+    }
+
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+}
