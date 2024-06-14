a0ffdb03231784959f5add3884d5f440670c90d4
minseok0507 <jms892812@gmail.com>
2024-06-14 17:25:09 +0900

    minseok detail service 작업

--- src/main/java/org/example/semiprojectpanda/mapperInter/ProductMapperInter.java
+++ src/main/java/org/example/semiprojectpanda/mapperInter/ProductMapperInter.java
@@ -1,7 +1,11 @@
 package org.example.semiprojectpanda.mapperInter;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.example.semiprojectpanda.dto.ProductDto;
 
 @Mapper
 public interface ProductMapperInter {
+    @Select("SELECT * FROM PRODUCT WHERE productnum = #{productnum}")
+    ProductDto getProductByProductnum(int productnum);
 }
--- src/main/java/org/example/semiprojectpanda/mapperInter/WishMapperInter.java
+++ src/main/java/org/example/semiprojectpanda/mapperInter/WishMapperInter.java
@@ -1,7 +1,20 @@
 package org.example.semiprojectpanda.mapperInter;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.*;
+import org.example.semiprojectpanda.dto.WishDto;
 
 @Mapper
 public interface WishMapperInter {
+    // Create
+    @Insert("INSERT INTO WISH (usernum, productnum) VALUES (#{usernum}, #{productnum})")
+    void insertWish(WishDto wish);
+
+
+    @Select("SELECT COUNT(*) FROM WISH WHERE usernum = #{usernum} AND productnum = #{productnum}")
+    int checkWishByUsernumAndProductnum(WishDto wish);
+
+    // Delete
+    @Delete("DELETE FROM WISH WHERE usernum = #{usernum} AND productnum = #{productnum}")
+    void deleteWish(WishDto wish);
+
 }
