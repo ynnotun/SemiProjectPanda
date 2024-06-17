3a3ba159b97fb1391ad3464feb7dd6a266e86f51
qhal5 <alfkk122@naver.com>
2024-06-17 17:23:25 +0900

    회원가입 시마이

--- src/main/java/org/example/semiprojectpanda/mapperInter/UserMapperInter.java
+++ src/main/java/org/example/semiprojectpanda/mapperInter/UserMapperInter.java
@@ -1,11 +1,23 @@
 package org.example.semiprojectpanda.mapperInter;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.example.semiprojectpanda.dto.UserDto;
+
+import java.util.Map;
 
 @Mapper
 public interface UserMapperInter {
     @Select("SELECT * FROM USER WHERE usernum = #{usernum}")
     UserDto findByUsernum(int usernum);
+    @Insert("""
+            insert into USER (useremail,userpassword,username,usernickname,userprofileimage,useraddress,userphonenumber,userbirthday,usercreatedate) values 
+            (#{useremail},#{userpassword},#{username},#{usernickname},#{userprofileimage},#{useraddress},#{userphonenumber},#{userbirthday},now())
+            """)
+    public void insertUser(UserDto dto);
+        @Update("update USER set userprofileimage=#{userprofileimage} where usernum=#{usernum}")
+    public void updatePhoto(Map<String, Object> map);
+
 }
