3845d2493221fcfb1c04096c55ffbdf5c97256cd
LBang98 <dlqudgus98@gmail.com>
2024-06-18 16:07:48 +0900

    test

--- src/main/java/org/example/semiprojectpanda/controller/chatting/ChatController.java
+++ src/main/java/org/example/semiprojectpanda/controller/chatting/ChatController.java
@@ -18,70 +18,86 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 public class ChatController {
 
-    private static String secretKey = "SEpsU0ZHYmRPRmhHQVpmZVVxSUt5WmhidENSQmRYZ1c=";
-    private static String apiUrl = "https://hfuj5gbnfk.apigw.ntruss.com/custom/v1/14820/662fd478ae538e1b0f002e1f97097070696872adc2748b8e2b46954a9ae0c115";
+    private static final Logger logger = Logger.getLogger(ChatController.class.getName());
+
+    private static final String SECRET_KEY = "dHRFRmF6ZE9qWFd2dnNNcnhGZ1BRS3J0VGZ0RUl2TEY=";
+    private static final String API_URL = "https://bgtp3dpg76.apigw.ntruss.com/custom/v1/14828/3965a753907d8398a2d872283a8b21282f3a930fcd429c433c23ca027dfd5c67";
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/public")
     public String sendMessage(@Payload String chatMessage) {
         try {
-            URL url = new URL(apiUrl);
+            logger.info("Sending message: " + chatMessage);
+            URL url = new URL(API_URL);
 
             String message = getReqMessage(chatMessage);
-            String encodeBase64String = makeSignature(message, secretKey);
+            logger.info("Request message: " + message);
+            String signature = makeSignature(message, SECRET_KEY);
+
+            logger.info("Generated signature: " + signature);
 
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("POST");
             con.setRequestProperty("Content-Type", "application/json;UTF-8");
-            con.setRequestProperty("X-NCP-CHATBOT_SIGNATURE", encodeBase64String);
+            con.setRequestProperty("X-NCP-CHATBOT_SIGNATURE", signature);
 
             con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.write(message.getBytes("UTF-8"));
-            wr.flush();
-            wr.close();
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(message.getBytes("UTF-8"));
+                wr.flush();
+            }
 
             int responseCode = con.getResponseCode();
-            if (responseCode == 200) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-                String decodedString;
-                StringBuilder jsonString = new StringBuilder();
-                while ((decodedString = in.readLine()) != null) {
-                    jsonString.append(decodedString);
+            logger.info("Response Code: " + responseCode);
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"))) {
+                    String decodedString;
+                    StringBuilder jsonString = new StringBuilder();
+                    while ((decodedString = in.readLine()) != null) {
+                        jsonString.append(decodedString);
+                    }
+                    JSONParser jsonparser = new JSONParser();
+                    JSONObject json = (JSONObject) jsonparser.parse(jsonString.toString());
+                    JSONArray bubblesArray = (JSONArray) json.get("bubbles");
+                    JSONObject bubbles = (JSONObject) bubblesArray.get(0);
+                    JSONObject data = (JSONObject) bubbles.get("data");
+                    String description = (String) data.get("description");
+                    logger.info("Response description: " + description);
+                    return description;
                 }
-                in.close();
-
-                JSONParser jsonparser = new JSONParser();
-                JSONObject json = (JSONObject) jsonparser.parse(jsonString.toString());
-                JSONArray bubblesArray = (JSONArray) json.get("bubbles");
-                JSONObject bubbles = (JSONObject) bubblesArray.get(0);
-                JSONObject data = (JSONObject) bubbles.get("data");
-                String description = (String) data.get("description");
-                System.out.println(description);
-                return description;
             } else {
-                return "Error: " + con.getResponseMessage();
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream(), "UTF-8"))) {
+                    String errorString;
+                    StringBuilder errorResponse = new StringBuilder();
+                    while ((errorString = in.readLine()) != null) {
+                        errorResponse.append(errorString);
+                    }
+                    logger.severe("Error Response: " + errorResponse.toString());
+                    return "Error: " + errorResponse.toString();
+                }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Internal Server Error", e);
             return "Internal Server Error: " + e.getMessage();
         }
     }
 
     public static String makeSignature(String message, String secretKey) {
         try {
-            byte[] secrete_key_bytes = Base64.decodeBase64(secretKey);
-            SecretKeySpec signingKey = new SecretKeySpec(secrete_key_bytes, "HmacSHA256");
+            byte[] secretKeyBytes = Base64.decodeBase64(secretKey);
+            SecretKeySpec signingKey = new SecretKeySpec(secretKeyBytes, "HmacSHA256");
             Mac mac = Mac.getInstance("HmacSHA256");
             mac.init(signingKey);
             byte[] rawHmac = mac.doFinal(message.getBytes("UTF-8"));
             return Base64.encodeBase64String(rawHmac);
         } catch (Exception e) {
-            e.printStackTrace();
+            Logger.getLogger(ChatController.class.getName()).log(Level.SEVERE, "Error while generating signature", e);
             return "";
         }
     }
@@ -94,25 +110,25 @@ public class ChatController {
             obj.put("userId", "U47b00b58c90f8e47428af8b7bddc1231heo2");
             obj.put("timestamp", timestamp);
 
-            JSONObject bubbles_obj = new JSONObject();
-            bubbles_obj.put("type", "text");
+            JSONObject bubblesObj = new JSONObject();
+            bubblesObj.put("type", "text");
 
-            JSONObject data_obj = new JSONObject();
-            data_obj.put("description", voiceMessage);
+            JSONObject dataObj = new JSONObject();
+            dataObj.put("description", voiceMessage);
 
-            bubbles_obj.put("type", "text");
-            bubbles_obj.put("data", data_obj);
+            bubblesObj.put("type", "text");
+            bubblesObj.put("data", dataObj);
 
-            JSONArray bubbles_array = new JSONArray();
-            bubbles_array.add(bubbles_obj);
+            JSONArray bubblesArray = new JSONArray();
+            bubblesArray.add(bubblesObj);
 
-            obj.put("bubbles", bubbles_array);
+            obj.put("bubbles", bubblesArray);
             obj.put("event", "send");
 
-            System.out.println(obj.toString());
+            logger.info("Request JSON: " + obj.toString());
             return obj.toString();
         } catch (Exception e) {
-            System.out.println("## Exception : " + e);
+            logger.log(Level.SEVERE, "Error while creating request message", e);
             return "";
         }
     }
