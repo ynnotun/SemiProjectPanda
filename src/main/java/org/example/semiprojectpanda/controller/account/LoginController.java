e16d09b0bac2b17d22ad0ef2c19e2da28e4a48c9
minseok0507 <jms892812@gmail.com>
2024-06-26 15:14:08 +0900

    ʅ(´・ᴗ・` )ʃ 로그인 회원가입 비밀번호 해시

--- src/main/java/org/example/semiprojectpanda/controller/account/LoginController.java
+++ src/main/java/org/example/semiprojectpanda/controller/account/LoginController.java
@@ -1,25 +1,19 @@
 package org.example.semiprojectpanda.controller.account;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import lombok.RequiredArgsConstructor;
+import org.example.semiprojectpanda.config.SHA256;
 import org.example.semiprojectpanda.dto.UserDto;
 import org.example.semiprojectpanda.naver.cloud.NcpObjectStorageService;
 import org.example.semiprojectpanda.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.IOException;
-import java.sql.Timestamp;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,7 +33,7 @@ public class LoginController {
     public String saveDate(
             @ModelAttribute UserDto dto,
             @RequestParam(required = false, value = "myfile", defaultValue="userprofileimage")MultipartFile myfile
-    ){
+    ) throws NoSuchAlgorithmException {
         if (!myfile.isEmpty()){
             //스토리지에 업로드
             String photo=storageService.uploadFile(buckname, folderName, myfile);
@@ -47,6 +41,8 @@ public class LoginController {
             System.out.println(photo);
         }
         System.out.println(dto);
+        SHA256 sha256 = new SHA256();
+        dto.setUserpassword(sha256.encrypt(dto.getUserpassword() + sha256.encrypt(dto.getUseremail())));
         //db 에 저장
         userService.insertMember(dto);
 
@@ -59,10 +55,12 @@ public class LoginController {
             @RequestParam String useremail,
             @RequestParam String userpassword,
             HttpSession session
-    ){
+    ) throws NoSuchAlgorithmException {
         Map<String,String> map=new HashMap<>();
+        SHA256 sha256 = new SHA256();
+        String password = sha256.encrypt(userpassword + sha256.encrypt(useremail));
         //로그인 상태
-        boolean loginStatus=userService.isLoginCheck(useremail, userpassword);
+        boolean loginStatus=userService.isLoginCheck(useremail, password);
         if (loginStatus){
             //아이디와 비번이 맞은경우
             map.put("status","success");
